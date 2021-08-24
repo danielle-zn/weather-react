5353e02aa7b4fe8fb1997c86a5e67a6e4990e017
danielle-zn <84985016+danielle-zn@users.noreply.github.com>
2021-08-24 15:42:26 -0600

    Installed bootstrap in terminal, and imported in index.js

--- src/App.js
+++ src/App.js
@@ -5,21 +5,23 @@ import SearchForm from "./SearchForm";
 function App() {
   return (
     <div className="App">
-      <div className="weather-app">
-        <SearchForm />
-        <br />
-        <Weather />
+      <div className="container">
+        <div className="weather-app">
+          <SearchForm />
+          <br />
+          <Weather />
+        </div>
+        <footer className=" mt-5">
+          <a
+            href="https://github.com/danielle-zn/weather-react.git"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code
+          </a>
+          , by Danielle Zamora Nieto
+        </footer>
       </div>
-      <footer className=" mt-5">
-        <a
-          href="https://github.com/danielle-zn/weather-react.git"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-source code
-        </a>
-        , by Danielle Zamora Nieto
-      </footer>
     </div>
   );
 }
