fa7609151b4e722754226f48e656ef3df90f7345
danielle-zn <84985016+danielle-zn@users.noreply.github.com>
2021-08-28 20:24:08 -0600

    Created WeatherForecastDay component

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,33 +1,36 @@
-import React from "react";
-import WeatherIcon from "./WeatherIcon";
+import React, { useState } from "react";
+import WeatherForecastDay from "./WeatherForecastDay";
 import "./WeatherForecast.css";
 import axios from "axios";
 
 export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
   function getForecast(response) {
     console.log(response.data);
+    setForecast(response.data.daily);
+    setLoaded(true);
   }
 
-  const apiKey = "0f2efbf7da0a99fa73f8d244bcbbe2f5";
-  const latitude = props.coordinates.lat;
-  const longitude = props.coordinates.lon;
-  const apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-
-  axios.get(apiUrl).then(getForecast);
-
-  return (
-    <div className="WeatherForecast">
-      <div className="row mt-4 text-center">
-        <div className="col">
-          <div className="forecast-day mb-2">Thu</div>
-          <WeatherIcon code="01d" size={35} />
-          <div className="forecast-temperatures">
-            <span className="forecast-temp-max">19°</span>
-
-            <span className="forecast-temp-min">10°</span>
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row mt-4 text-center">
+          <div className="col">
+            <WeatherForecastDay data={forecast[0]} />
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = "0f2efbf7da0a99fa73f8d244bcbbe2f5";
+    let latitude = props.coordinates.lat;
+    let longitude = props.coordinates.lon;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(getForecast);
+
+    return null;
+  }
 }
